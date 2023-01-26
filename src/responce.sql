2990b5ffa75556916b296fa2d0d48085482811c0
ArhDemetriy <ArhDemetriy@GMail.com>
2023-01-26 12:17:00 +0800

    dev

--- src/responce.sql
+++ src/responce.sql
@@ -1,2 +1,22 @@
 -- начало и конец первого диапазона с максимальным кол-вом одновременных процессов
 
+SELECT startDates.cid as cid, start, end
+FROM (
+    (
+        SELECT *
+        FROM (
+            SELECT cid, date as 'start'
+            FROM events
+            WHERE type='start'
+        )
+    ) as 'startDates'
+    JOIN (
+        SELECT *
+        FROM (
+            SELECT cid, date as 'end'
+            FROM events
+            WHERE type='end'
+        )
+    ) as 'endDates'
+    ON startDates.cid = endDates.cid
+)
