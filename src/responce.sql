4236f719dfc5aeb48ce7a49ba020e1b6b74546f7
ArhDemetriy <ArhDemetriy@GMail.com>
2023-01-30 14:20:16 +0800

    все данные для вычисления количества тасков

--- src/responce.sql
+++ src/responce.sql
@@ -45,27 +45,57 @@ FROM (
     ON endDates.date >= startDates.date
 )
 
--- количество тасков начавшихся на момент окончания указанного таска
-SELECT endDates.cid as 'cid', endDates.date as 'date', count() as 'count'
+
+SELECT startedDates.cid as 'cid', startedDates.date as 'start', endestDates.date as 'end', startedDates.count, endestDates.count
 FROM (
-    (
-        SELECT *
-        FROM (
-            SELECT cid, date
-            FROM events
-            WHERE type='end'
-        )
-    ) as 'endDates'
-    LEFT JOIN
-    (
-        SELECT *
-        FROM (
-            SELECT cid, date
-            FROM events
-            WHERE type='start'
-        )
-    ) as 'startDates'
-    ON endDates.date >= startDates.date
-)
-GROUP BY endDates.cid
-ORDER BY endDates.date
+    -- количество тасков начавшихся на момент окончания указанного таска
+    SELECT endDates.cid as 'cid', endDates.date as 'date', count() as 'count'
+    FROM (
+        (
+            SELECT *
+            FROM (
+                SELECT cid, date
+                FROM events
+                WHERE type='end'
+            )
+        ) as 'endDates'
+        LEFT JOIN
+        (
+            SELECT *
+            FROM (
+                SELECT cid, date
+                FROM events
+                WHERE type='start'
+            )
+        ) as 'startDates'
+        ON endDates.date >= startDates.date
+    )
+    GROUP BY endDates.cid
+) as 'startedDates'
+LEFT JOIN
+(
+    -- количество тасков окончившихся до окончания указанного таска
+    SELECT endDates.cid as 'cid', endDates.date as 'date', count(longTimeEnded.cid) as 'count'
+    FROM (
+        (
+            SELECT *
+            FROM (
+                SELECT cid, date
+                FROM events
+                WHERE type='end'
+            )
+        ) as 'endDates'
+        LEFT JOIN
+        (
+            SELECT *
+            FROM (
+                SELECT cid, date
+                FROM events
+                WHERE type='end'
+            )
+        ) as 'longTimeEnded'
+        ON endDates.date > longTimeEnded.date
+    )
+    GROUP BY endDates.cid
+) as 'endestDates'
+ON startedDates.cid = endestDates.cid
