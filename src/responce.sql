fe05af4c38f1f43f01cd0faa72df7a4cc2a0d184
ArhDemetriy <ArhDemetriy@GMail.com>
2023-01-30 12:38:33 +0800

    dev

--- src/responce.sql
+++ src/responce.sql
@@ -20,3 +20,36 @@ FROM (
     ) as 'endDates'
     ON startDates.cid = endDates.cid
 )
+
+
+SELECT startDates.cid as cid, start, end
+SELECT *, count(endDates.date >= startDates.date) as 'count'
+FROM (
+    (
+        SELECT *
+        FROM (
+            SELECT cid, date
+            FROM events
+            WHERE type='end'
+        )
+    ) as 'endDates'
+    LEFT JOIN
+    (
+        SELECT *
+        FROM (
+            SELECT cid, date
+            FROM events
+            WHERE type='start'
+        )
+    ) as 'startDates'
+    ON endDates.date >= startDates.date
+)
+
+
+SELECT count(date)
+FROM (
+        SELECT cid, date
+        FROM events
+        WHERE type='start'
+    )
+GROUP BY cid < 5
